b3c72850ebafcbabc17435c4cacb34dab6f99ddd
scrapgists <asishbm98@gmail.com>
2019-08-18 17:57:18 +0530

    Templates they say

--- DJANGO/PROJECT1/firstapp/views.py
+++ DJANGO/PROJECT1/firstapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("HELLO, World. You are at the poll index")
+    my_dict = {'insert_me':"Hello I am views.py!"}
+    return render(request,'index.html',context=my_dict)
